08f1b6f22f12c8330adfae1d4b5d5f76382905f3
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-09-18 13:42:39 -0400

    added fetch for Adzuna API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var apiKeyZip = 
-var apiKeyIndeed = 
-
+var apiKeyJooble = "59249837-731a-40d4-8f77-87656a08d596"
+var apiKeyAdzuna = "a340c7044ad462ad2595c48c2fc727af"
+var appIdAdzuna = "16a1e151"
 
 var formInputArr = JSON.parse(localStorage.getItem("formInputObj")) || [];
 var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryObj")) || [];
 
 var jobTitleEl = document.querySelector("#job-title");
-// console.log(jobTitleEl)
+var cityEl = document.querySelector("#city")
+var radiusEl = document.querySelector("#radius")
 var searchBtnLandingEl = document.querySelector("#search-btn-landing")
 
 function searchJobHandler (event) {
@@ -17,38 +18,60 @@ function searchJobHandler (event) {
         return;
     }
     else {
-
         var jobTitle = $("#job-title")
             .val()
             .trim();
         var city = $("#city")
             .val()
             .trim();
-        var state = $("#state-selector")
-            .val()
-            .trim();
-            // console.log(state)
+            // console.log(city)
+            if (!cityEl.value) {
+                city = ""
+            }
         var radius = $("#radius")
             .val()
             .trim();
-            // console.log(radius)
+            if (!radiusEl.value) {
+                radius = ""
+            }
         var formObj = {
             titleForm: jobTitle,
             cityForm: city,
-            stateForm: state,
             radiusForm: radius
         };
-        // console.log(formObj)
+        // console.log(formObj.titleForm)
         formInputArr.push(formObj);
         // console.log(formInputArr)
         localStorage.setItem("formInputObj", JSON.stringify(formInputArr));
     };
-        getJobsZip(formObj);
-        getJobsIndeed(formObj);
+        // getJobsJooble(formObj);
+        getJobsAdzuna(formObj);
 };
 
-function getJobsMonster (formObj) {
+// function getJobsJooble (formObj) {
+//     var apiUrlJooble = "https://jooble.org/api/" + apiKeyJooble + "/?keywords=" + formObj.titleForm;
+//     fetch(apiUrlJooble)
+//         .then(function(response) {
+//             if (response.ok) {
+//                 response.json().then(function(data) {
+//                     console.log(response)
+//                     console.log(data)
+//                 })
+//             }
+//         })
+// };
 
-};
+function getJobsAdzuna (formObj) {
+    var apiUrlAdzuna = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=" + appIdAdzuna + "&app_key=" + apiKeyAdzuna + "&what=" + formObj.titleForm + "&where=" + formObj.cityForm + "&distance=" + formObj.radiusForm
+    fetch(apiUrlAdzuna)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(response)
+                console.log(data)
+            })
+        }
+    })
+}
 
 searchBtnLandingEl.addEventListener("click", searchJobHandler);
