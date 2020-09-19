e6e9b32e7e01ced1e839ec77bc107f5e513b151f
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-09-19 16:52:36 -0400

    finsihed fetch for api's logic and started logic for getting specific data and then displaying it on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,22 +44,38 @@ function searchJobHandler (event) {
         // console.log(formInputArr)
         localStorage.setItem("formInputObj", JSON.stringify(formInputArr));
     };
-        // getJobsJooble(formObj);
+        getJobsJooble(formObj);
         getJobsAdzuna(formObj);
 };
 
-// function getJobsJooble (formObj) {
-//     var apiUrlJooble = "https://jooble.org/api/" + apiKeyJooble + "/?keywords=" + formObj.titleForm;
-//     fetch(apiUrlJooble)
-//         .then(function(response) {
-//             if (response.ok) {
-//                 response.json().then(function(data) {
-//                     console.log(response)
-//                     console.log(data)
-//                 })
-//             }
-//         })
-// };
+function getJobsJooble (formObj) {
+    var url = "https://jooble.org/api/";
+    var key = apiKeyJooble;
+    var params = "{keywords: '" + formObj.titleForm + "', location: '" + formObj.cityForm + "', radius: '" + formObj.radiusForm + "'}"
+
+    //create xmlHttpRequest object
+    var http = new XMLHttpRequest();
+    //open connection. true - asynchronous, false - synchronous
+    http.open("POST", url + key, true);
+
+    //Send the proper header information
+    http.setRequestHeader("Content-type", "application/json");
+        
+    //Callback when the state changes
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            var data = http.response;
+            var jsonResponse = JSON.parse(data);
+            // console.log(jsonResponse);
+            var jobs = jsonResponse.jobs;
+            // console.log(formObj)
+            // console.log(jobs);
+            displayJobsJooble(formObj, jobs);
+        }
+    }
+    //Send request to server
+    http.send(params);
+};
 
 function getJobsAdzuna (formObj) {
     var apiUrlAdzuna = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=" + appIdAdzuna + "&app_key=" + apiKeyAdzuna + "&what=" + formObj.titleForm + "&where=" + formObj.cityForm + "&distance=" + formObj.radiusForm
@@ -67,11 +83,50 @@ function getJobsAdzuna (formObj) {
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(response)
+                // console.log(response)
                 console.log(data)
-            })
+                displayJobsAdzuna(formObj, data)
+            });
+        }
+        else {
+            console.log("Error: Input " + response.statusText)
         }
     })
 }
+
+function displayJobsJooble(formObj, jobs) {
+    for (var i = 0; i < jobs.length;i++) {
+        var jobTitle = jobs[i].title
+        // console.log(jobTitle)
+        var jobLocation = jobs[i].location
+        // console.log(jobLocation)
+    };
+};
+
+function displayJobsAdzuna(formObj, data) {
+    var jobResults = data.results
+    console.log(jobResults)
+    for (var i = 0; i < jobResults.length; i++) {
+        var jobTitleResult = jobResults[i].title
+        const regex = /(<([^>]+)>)/ig;
+        const jobTitle = jobTitleResult.replace(regex,'');
+        // console.log(jobTitle)
+        var location = jobResults[i].location
+        var area = location.area
+        var locationName = area[3] + " " + area[1]
+        if (!area[3] && !area[1]) {
+            var locationName = area[0]
+            // console.log(locationName)
+        }
+        else if (!area[3]) {
+            var locationName = area[1]
+            // console.log(state)
+        }
+        console.log(locationName)
+    }
+}
+
+
+
 
 searchBtnLandingEl.addEventListener("click", searchJobHandler);
